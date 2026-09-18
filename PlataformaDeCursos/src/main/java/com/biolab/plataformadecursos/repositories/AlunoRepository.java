9fd686ae30c9cba33cbbde5f54bc6cbb3b5f2e0e
Vickky <liviav987@gmail.com>
2026-09-18 10:34:40 -0300

    Delete PlataformaDeCursos directory

--- PlataformaDeCursos/src/main/java/com/biolab/plataformadecursos/repositories/AlunoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.biolab.plataformadecursos.repositories;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.biolab.plataformadecursos.entities.Aluno;
-
-@Repository
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-}
--- PlataformaDeCursos/src/main/java/com/biolab/plataformadecursos/repositories/CursoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.biolab.plataformadecursos.repositories;
-
-import org.springframework.stereotype.Repository;
-import com.biolab.plataformadecursos.entities.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface CursoRepository extends JpaRepository<Curso, Long> {
-}
