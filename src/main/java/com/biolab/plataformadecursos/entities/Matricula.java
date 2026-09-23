3e3a6b5bb9ec14a09330a0fa8c838d079e752719
Vickky <liviav987@gmail.com>
2026-09-23 09:55:48 -0300

    Update Matricula.java
    Added comments to clarify the purpose of annotations and fields.

--- src/main/java/com/biolab/plataformadecursos/entities/Matricula.java
+++ src/main/java/com/biolab/plataformadecursos/entities/Matricula.java
@@ -8,10 +8,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter //cria automaticamente metodo de leitura
+@Setter //cria automatico o metodo de modificação
+@NoArgsConstructor //construtor sem parametros
+@AllArgsConstructor //construtor com todos parametros
 public class Matricula {
 
     @Id
@@ -20,11 +20,11 @@ public class Matricula {
 
     @ManyToOne
     @JoinColumn(name = "aluno_id")
-    @JsonIgnoreProperties("matriculas")
+    @JsonIgnoreProperties("matriculas") //evita o looping
     private Aluno aluno;
 
     @ManyToOne
     @JoinColumn(name = "curso_id")
-    @JsonIgnoreProperties("matriculas")
+    @JsonIgnoreProperties("matriculas") //evita o looping
     private Curso curso;
 }
