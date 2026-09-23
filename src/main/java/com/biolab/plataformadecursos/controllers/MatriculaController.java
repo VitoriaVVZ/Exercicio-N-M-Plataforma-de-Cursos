e15657ca73fd5532f599bd374b19fc71ac4709f1
Vickky <liviav987@gmail.com>
2026-09-23 09:13:09 -0300

    add Matricula to Controllers

---
+++ src/main/java/com/biolab/plataformadecursos/controllers/MatriculaController.java
@@ -0,0 +1,45 @@
+package com.biolab.plataformadecursos.controllers;
+
+import com.biolab.plataformadecursos.DTOs.MatriculaDTO;
+import com.biolab.plataformadecursos.entities.Matricula;
+import com.biolab.plataformadecursos.services.MatriculaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/matricula")
+public class MatriculaController {
+
+    private final MatriculaService matriculaService;
+
+    public MatriculaController(MatriculaService matriculaService) {
+        this.matriculaService = matriculaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> matricular(@RequestBody MatriculaDTO dto) {
+        String resposta = matriculaService.matricular(dto.getAlunoId(), dto.getCursoId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> removerMatricula(@PathVariable Long id) {
+        String resposta = matriculaService.removerMatricula(id);
+        return ResponseEntity.ok(resposta);
+    }
+
+    @GetMapping("/aluno/{alunoId}")
+    public ResponseEntity<List<Matricula>> buscarMatriculasDoAluno(@PathVariable Long alunoId) {
+        List<Matricula> matriculas = matriculaService.buscarMatriculasDoAluno(alunoId);
+        return ResponseEntity.ok(matriculas);
+    }
+
+    @GetMapping("/curso/{cursoId}")
+    public ResponseEntity<List<Matricula>> buscarMatriculasDoCurso(@PathVariable Long cursoId) {
+        List<Matricula> matriculas = matriculaService.buscarMatriculasDoCurso(cursoId);
+        return ResponseEntity.ok(matriculas);
+    }
+}
