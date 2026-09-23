15e691eef7042d0e3a1d786c127e49476744b1ed
Vickky <liviav987@gmail.com>
2026-09-23 09:12:28 -0300

    add Matricula to Services

---
+++ src/main/java/com/biolab/plataformadecursos/services/MatriculaService.java
@@ -0,0 +1,56 @@
+package com.biolab.plataformadecursos.services;
+
+import com.biolab.plataformadecursos.DTOs.MatriculaDTO;
+import com.biolab.plataformadecursos.entities.Aluno;
+import com.biolab.plataformadecursos.entities.Curso;
+import com.biolab.plataformadecursos.entities.Matricula;
+import com.biolab.plataformadecursos.repositories.AlunoRepository;
+import com.biolab.plataformadecursos.repositories.CursoRepository;
+import com.biolab.plataformadecursos.repositories.MatriculaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MatriculaService {
+
+    private final MatriculaRepository matriculaRepository;
+    private final AlunoRepository alunoRepository;
+    private final CursoRepository cursoRepository;
+
+    public MatriculaService(MatriculaRepository matriculaRepository,
+                            AlunoRepository alunoRepository,
+                            CursoRepository cursoRepository) {
+        this.matriculaRepository = matriculaRepository;
+        this.alunoRepository = alunoRepository;
+        this.cursoRepository = cursoRepository;
+    }
+
+    public String matricular(Long alunoId, Long cursoId) {
+        Aluno aluno = alunoRepository.findById(alunoId).orElseThrow();
+        Curso curso = cursoRepository.findById(cursoId).orElseThrow();
+
+        Matricula matricula = new Matricula();
+        matricula.setAluno(aluno);
+        matricula.setCurso(curso);
+
+        matriculaRepository.save(matricula);
+        return "Aluno matriculado no curso com sucesso!!";
+    }
+
+    public String removerMatricula(Long id) {
+        Matricula matricula = matriculaRepository.findById(id).orElseThrow();
+        matriculaRepository.deleteById(matricula.getId());
+        return "Matricula removida com sucesso!!";
+    }
+
+    public List<Matricula> buscarMatriculasDoAluno(Long alunoId) {
+        Aluno aluno = alunoRepository.findById(alunoId).orElseThrow();
+        return aluno.getMatriculas();
+    }
+
+    public List<Matricula> buscarMatriculasDoCurso(Long cursoId) {
+        Curso curso = cursoRepository.findById(cursoId).orElseThrow();
+        return curso.getMatriculas();
+    }
+}
