86125558ca4fb52790d86e6e6f389ad6fa1a26f2
Vickky <liviav987@gmail.com>
2026-09-23 09:11:13 -0300

    add Matricula to Repositories

---
+++ src/main/java/com/biolab/plataformadecursos/repositories/MatriculaRepository.java
@@ -0,0 +1,8 @@
+package com.biolab.plataformadecursos.repositories;
+
+import com.biolab.plataformadecursos.entities.Matricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
+
+}
